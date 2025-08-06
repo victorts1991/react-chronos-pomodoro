de71a6777a41f6eb42d8dabd245353f5a5c91f89
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2025-08-06 08:28:38 -0300

    add some components, countdown, cycles and defaultbutton

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { Logo } from './components/Logo';
 import './styles/theme.css'
 import './styles/global.css'
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 
 function App() {
@@ -14,6 +19,35 @@ function App() {
             </Container>
             <Container>
                 <Menu />
+            </Container>
+            <Container>
+                <CountDown />
+            </Container>
+            <Container>
+                <form className='form'>
+                    <div className='formRow'>
+                        <DefaultInput 
+                            id="meuInput" 
+                            type="text"
+                            labelText='Tarefa'
+                            placeholder='Digite algo'
+                        />
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Nesse ciclo descanse por 5 minutos.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <Cycles />
+                    </div>
+
+                    <div className='formRow'>
+                        <DefaultButton 
+                            icon={<PlayCircleIcon />}
+                        />
+                    </div>
+                </form>
             </Container>
         </>
     );
